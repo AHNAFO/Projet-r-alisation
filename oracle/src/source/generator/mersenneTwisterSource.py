b92ad9da17b9879ae367a3cee9f126bf37a3830f
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2023-01-24 14:25:29 +0100

    rename all class

---
+++ oracle/src/source/generator/mersenneTwisterSource.py
@@ -0,0 +1,6 @@
+import Source
+
+class MersenneTwisterSource(Source):
+
+    def __init__(self, length):
+        self.length = length
---
+++ oracle/src/source/source.py
@@ -0,0 +1,14 @@
+
+# cette classe retourne un tableau de n nombre random.
+# le nombre n est defini par les besoins de son predicteur respectif
+
+
+class Source:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def getTab(lengthTab):
+        return 
+    
+
